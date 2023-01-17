76302b320df3985b9d5fa9ebd775e6173f0cf6af
Diego Alonso Norabuena Rojas <sdiego-2000@hotmail.com>
2023-01-17 16:35:47 -0500

    creando proyecto

---
+++ src/main/java/com/platzi/marker/PlatziMarkerApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.marker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziMarkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziMarkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platzi/marker/PlatziMarkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.marker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziMarkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
